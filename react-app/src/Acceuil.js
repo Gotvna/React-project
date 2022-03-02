0b02b41a8781faf536fd1dd1f8c463b758654037
Gotvna <nvogt@gaming.tech>
2022-03-02 15:39:44 +0100

    Site v1.0

---
+++ react-app/src/Acceuil.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Acceuil extends Component {
+    render() {
+        return(
+            <div>
+                <p>Fuck the police</p>
+            </div>
+        );
+    }
+}
+export default Acceuil;
