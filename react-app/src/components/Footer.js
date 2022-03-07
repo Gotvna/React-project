195aec13e2e4f1e71c7a38aa240b982cda902b23
Gotvna <nvogt@gaming.tech>
2022-03-07 11:11:02 +0100

    update

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Footer = () => <footer className="">
+const Footer = () => <footer className="fixed-bottom">
     <div className="footer-copyright text-center py-3">© 2020 Copyright:
         <p>Asian Shop</p>
     </div>
